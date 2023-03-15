d2ec5946ceeb4da24d4c4b38474935beab211c5d
Kerimov056 <ulvisk@code.edu.az>
2023-03-14 20:57:25 -0700

    operator

---
+++ Operator overloading/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Operator_overloading;
+
+public class Person
+{
+    public string name { get; set; }
+    public string surname { get; set; }
+    public int age { get; set; }
+
+    public static bool operator >(Person a, Person b)
+    {
+        return a.age > b.age;
+    }
+    public static bool operator <(Person a, Person b)
+    {
+        return a.age < b.age;
+    }
+}
---
+++ Operator overloading/Program.cs
@@ -0,0 +1,38 @@
+﻿using Operator_overloading;
+
+Person human1 = new Person()
+{
+    name = "Ulvi",
+    surname = "Kerimov",
+    age = 49
+};
+Person human2 = new Person()
+{
+    name = "Miribrahim",
+    surname = "Kerimov",
+    age = 10
+};
+Person human3 = new Person()
+{
+    name = "Sadiq",
+    surname = "Selimov",
+    age = 29
+};
+Person[] p1 = new Person[] { human1, human2, human3 };
+
+for (int i = 0; i < p1.Length; i++)
+{
+    for (int j = i + 1; j < p1.Length; j++)
+    {
+        if (p1[i].age > p1[j].age)
+        {
+            p1[i].age = p1[j].age + p1[i].age;
+            p1[j].age = p1[i].age - p1[j].age;
+            p1[i].age = p1[i].age - p1[j].age;
+        }
+    }
+}
+foreach (var item in p1)
+{
+    Console.WriteLine(item.age);
+}
